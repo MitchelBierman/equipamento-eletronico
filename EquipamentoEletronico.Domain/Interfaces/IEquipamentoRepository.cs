033cd10f42ae85ad55dd44e6c132f5632a63ac2f
MitchelBierman <mitchel.fb@hotmail.com>
2025-02-21 13:53:56 -0300

    feat: solução criada e projetos estruturados; referências incluídas; inclusão pacote xUnit para testes

---
+++ EquipamentoEletronico.Domain/Interfaces/IEquipamentoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipamentoEletronico.Domain.Interfaces
+{
+    internal interface IEquipamentoRepository
+    {
+    }
+}
