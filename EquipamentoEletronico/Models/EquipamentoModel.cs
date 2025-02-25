b096e76d07b31d91d2be447407a4985e958987b8
MitchelBierman <mitchel.fb@hotmail.com>
2025-02-25 13:07:50 -0300

    feat: novo ajuste em gitignore

---
+++ EquipamentoEletronico/Models/EquipamentoModel.cs
@@ -0,0 +1,26 @@
+﻿using EquipamentoEletronico.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace EquipamentoEletronico.API.Models
+{
+    public class EquipamentoModel
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Tipo { get; set; }
+        [Range(0, 10000, ErrorMessage = "A quantidade deve ser 0 e 10000.")]
+        public int QtdEmEstoque { get; set; }
+        public DateTime DataInclusao { get; set; }
+
+        public Equipamento ToEntity()
+        {
+            return new Equipamento
+            {
+                Nome = this.Nome,
+                Tipo = this.Tipo,
+                QtdEmEstoque = this.QtdEmEstoque,
+                DataInclusao = DateTime.Now
+            };
+        }
+    }
+}
